a8dcb4a07c6277d75d21d499957824b3dbd3093a
bijayee-saswata <saswat.sipun@gmail.com>
2018-09-10 00:45:55 +0530

    socket connection between client and server established

--- index.js
+++ index.js
@@ -1,8 +1,17 @@
 var express = require('express');
-var app = express();
+var socket = require('socket.io');
 
+//app setup
+var app = express();
 var server = app.listen(5000, function () {
   console.log("listening to port 5000");
 });
 
+//static file
 app.use(express.static('public'));
+
+//Socket.io setup
+var io = socket(server);
+io.on('connection', function(socket){
+  console.log("connection established.. ID: " + socket.id);
+});
